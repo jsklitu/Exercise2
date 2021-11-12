73f7069e97a99df52d225f25a9f1492453d855d0
Thea <tkjeldsmark@gmail.com>
2021-11-12 10:19:27 +0100

    Read ips and ports from file

--- main.go
+++ main.go
@@ -1,75 +1,54 @@
 package main
 
 import (
-	//"context"
-	"fmt" //så er vi klar til debug lol
+	"bufio"
+	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"time"
-
-	//proto "Exercise2/proto"
-
-	serf "github.com/hashicorp/serf/serf"
-	//"google.golang.org/grpc/profiling/proto"
 )
 
-func main() {
-	advertisePort := "5001"
-	bindPort := os.Args[1]
-	fmt.Println(advertisePort)
-	fmt.Println(bindPort)
-	cluster, err := setupCluster(
-		os.Getenv("ADVERTISE_ADDR"),
-		os.Getenv("CLUSTER_ADDR"),
-		advertisePort,
-		bindPort)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	time.Sleep(3200 * time.Second)
-	defer cluster.Leave()
+type Client struct {
+	id        int
+	ipAndPort string
+	timeStamp int
+	queue     []string
 }
 
-func setupCluster(advertiseAddr string, bindAdd string, advertisePort string, bindPort string) (*serf.Serf, error) {
-	conf := serf.DefaultConfig()
-	conf.Init()
-	conf.MemberlistConfig.AdvertiseAddr = "127.0.0.1"
-	conf.MemberlistConfig.Name = bindPort
+func main() {
+	// Get the client port in the form ip:port
+	clientIpAndPort := os.Args[1]
 
-	bindPortInt, err := strconv.Atoi(bindPort)
+	clients := getOtherClientRoutes("ipsAndPorts.txt", clientIpAndPort)
+	for j := 0; j < len(clients); j++ {
+		fmt.Println(clients[j])
+	}
+}
+
+func getOtherClientRoutes(fileName string, clientIpAndPort string) []string {
+	// Open file
+	f, err := os.Open(fileName)
 	if err != nil {
+		log.Fatal("Could not read file ", err)
+	}
+	// Close the file at the end of the program
+	defer f.Close()
+
+	// Read file line by line
+	scanner := bufio.NewScanner(f)
+
+	var routes []string
+	for scanner.Scan() {
+		ipAndPort := scanner.Text()
+		// Do not include the client's own port
+		if ipAndPort != clientIpAndPort {
+			routes = append(routes, ipAndPort)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-
-	advertisePortInt, err := strconv.Atoi(advertisePort)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	conf.MemberlistConfig.AdvertisePort = advertisePortInt
-	conf.MemberlistConfig.BindPort = bindPortInt
-
-	// Try to create a cluster
-	cluster, error := serf.Create(conf)
-	if error != nil {
-		log.Printf("Could not create a cluster :(")
-	}
-
-	newBind := bindPortInt - 1
-	fmt.Println(bindPortInt, newBind)
-
-	_, error = cluster.Join([]string{"127.0.0.1:" + strconv.Itoa(newBind)}, false)
-
-	if error != nil {
-		log.Printf("Could not join existing cluster - so creating own :)")
-	} else {
-		fmt.Println("I have joined")
-	}
-
-	return cluster, nil
+	return routes
 }
 
 /*func Request(ctx context.Context, in *proto.Message) (*proto.Close, error) {
