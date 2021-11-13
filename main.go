995b86f1f73a9856a89dca76ed70c6d253d42594
Thea <tkjeldsmark@gmail.com>
2021-11-13 13:24:59 +0100

    Remove unnecessary

--- main.go
+++ main.go
@@ -2,12 +2,7 @@ package main
 
 import (
 	proto "Exercise2/grpc"
-	"bufio"
 	"context"
-	"fmt"
-	//"google.golang.org/grpc"
-	"log"
-	"os"
 )
 
 type Client struct {
@@ -19,39 +14,7 @@ type Client struct {
 }
 
 func main() {
-	// Get the client port in the form ip:port
-	clientIpAndPort := os.Args[1]
 
-	otherClients := getOtherClientRoutes("ipsAndPorts.txt", clientIpAndPort)
-	for j := 0; j < len(otherClients); j++ {
-		fmt.Println(otherClients[j])
-	}
-}
-
-func getOtherClientRoutes(fileName string, clientIpAndPort string) []string {
-	// Open file
-	f, err := os.Open(fileName)
-	if err != nil {
-		log.Fatal("Could not read file ", err)
-	}
-	// Close the file at the end of the program
-	defer f.Close()
-
-	// Read file line by line
-	scanner := bufio.NewScanner(f)
-
-	var routes []string
-	for scanner.Scan() {
-		ipAndPort := scanner.Text()
-		// Do not include the client's own port
-		if ipAndPort != clientIpAndPort {
-			routes = append(routes, ipAndPort)
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-	return routes
 }
 
 func (c *Client) Request(ctx context.Context, in *proto.Message) (*proto.Close, error) {
